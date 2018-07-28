79cb0984b2640997b299b750cc1f3c7274c10d6b
cristian <c.patino@utp.edu.co>
2018-07-28 15:15:22 -0500

    Se agrego titulos de Ventana_Mejorada

--- Ventana_Mejorada.py
+++ Ventana_Mejorada.py
@@ -8,7 +8,7 @@ HEIGHT=400
 def main():
 	pygame.init()
 	pantalla = pygame.display.set_mode((WIDTH,HEIGHT))
-	pygame.display.set_caption("Pygame")
+	pygame.display.set_caption("Ventana Mejorada")
 	reloj = pygame.time.Clock()
 	while True:	
 		for evento in pygame.event.get():
